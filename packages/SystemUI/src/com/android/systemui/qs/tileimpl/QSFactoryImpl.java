c9762de6954ac8cb332a14f0c4b401d95b1f43e5
ezio84 <brabus84@gmail.com>
2017-09-12 14:20:27 +0800

    Add partial/full screenshot QS tile single tap to choose the mode, long press to fire
    thanks @kantjer for the icons!

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -39,6 +39,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -101,6 +102,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new NfcTile(mHost);
             case "caffeine":
                 return new CaffeineTile(mHost);
+            case "screenshot":
+                return new ScreenshotTile(mHost);
         }
 
         // Intent tiles.
