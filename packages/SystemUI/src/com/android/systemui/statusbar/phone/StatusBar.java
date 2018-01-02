e37bac3df85b1244faff78d7b18497ab54a22670
Chris <chris@cypheros.co>
2018-01-01 22:45:19 -0500

    base: Support black theme
    Adds a system black theme as an optional choice
    
    Change-Id: I651be15236c886f4725c2a5a5a50ca882683f397

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2135,6 +2135,17 @@ public class StatusBar extends SystemUI implements DemoMode,
         return themeInfo != null && themeInfo.isEnabled();
     }
 
+    public boolean isUsingBlackTheme() {
+        OverlayInfo themeInfo = null;
+        try {
+            themeInfo = mOverlayManager.getOverlayInfo("com.android.system.theme.black",
+                    mLockscreenUserManager.getCurrentUserId());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return themeInfo != null && themeInfo.isEnabled();
+    }
+
     @Nullable
     public View getAmbientIndicationContainer() {
         return mAmbientIndicationContainer;
@@ -3930,6 +3941,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         int userThemeSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.SYSTEM_THEME_STYLE, 0, mLockscreenUserManager.getCurrentUserId());
         boolean useDarkTheme = false;
+        boolean useBlackTheme = false;
         if (userThemeSetting == 0) {
             // The system wallpaper defines if QS should be light or dark.
             WallpaperColors systemColors = mColorExtractor
@@ -3938,6 +3950,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     && (systemColors.getColorHints() & WallpaperColors.HINT_SUPPORTS_DARK_THEME) != 0;
         } else {
             useDarkTheme = userThemeSetting == 2;
+            useBlackTheme = userThemeSetting == 3;
         }
         if (isUsingDarkTheme() != useDarkTheme) {
             try {
@@ -3947,6 +3960,18 @@ public class StatusBar extends SystemUI implements DemoMode,
                         useDarkTheme, mLockscreenUserManager.getCurrentUserId());
                 mOverlayManager.setEnabled("com.android.settings.theme.dark",
                         useDarkTheme, mLockscreenUserManager.getCurrentUserId());
+            } catch (RemoteException e) {
+                Log.w(TAG, "Can't change theme", e);
+            }
+        }
+        if (isUsingBlackTheme() != useBlackTheme) {
+            try {
+                mOverlayManager.setEnabled("com.android.system.theme.black",
+                        useBlackTheme, mLockscreenUserManager.getCurrentUserId());
+                mOverlayManager.setEnabled("com.android.systemui.theme.black",
+                        useDarkTheme, mLockscreenUserManager.getCurrentUserId());
+                mOverlayManager.setEnabled("com.android.settings.theme.black",
+                        useBlackTheme, mLockscreenUserManager.getCurrentUserId());
             } catch (RemoteException e) {
                 Log.w(TAG, "Can't change theme", e);
             }
